ea51bbbbef69b8eedda4c92ff539ea96b039dacd
kao678 <kaobas2536@gmail.com>
2026-01-23 22:59:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,12 @@
 module.exports = {
   LINE_TOKEN: process.env.LINE_TOKEN,
-  LINE_SECRET: process.env.LINE_SECRET
+  LINE_SECRET: process.env.LINE_SECRET,
+
+  // ห้อง
+  PLAY_GROUP_ID: "PLAY_GROUP_ID_HERE",
+  DEPOSIT_GROUP_ID: "DEPOSIT_GROUP_ID_HERE",
+  ADMIN_GROUP_ID: "ADMIN_GROUP_ID_HERE",
+
+  // รูปลูกเต๋า (ถ้าใช้)
+  DICE_URL: "https://your-domain/dice"
 };
