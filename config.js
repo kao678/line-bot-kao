ee08d03d2f60df5f9b52e84503104dc1d34b951b
kao678 <kaobas2536@gmail.com>
2026-01-23 20:00:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  PLAY_GROUP: "Gxxxxxxxx_play",
-  DEPOSIT_GROUP: "Gxxxxxxxx_deposit",
-  ADMIN_GROUP: "Gxxxxxxxx_admin",
-
-  RATE: {
-    NORMAL_WIN: 1,
-    NORMAL_LOSE: 3,
-    SPECIAL_456: 25,
-    TRIPLE: 100
-  }
+  CHANNEL_ACCESS_TOKEN: process.env.LINE_TOKEN,
+  CHANNEL_SECRET: process.env.LINE_SECRET
 };
