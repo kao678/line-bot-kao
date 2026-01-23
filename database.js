e33600e25751dc99f5a2a433f29478129000595b
kao678 <kaobas2536@gmail.com>
2026-01-23 21:55:46 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,13 +1,5 @@
 const fs = require("fs");
-
 const DB_FILE = "./data.json";
 
-function loadDB() {
-  return JSON.parse(fs.readFileSync(DB_FILE, "utf8"));
-}
-
-function saveDB(db) {
-  fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2));
-}
-
-module.exports = { loadDB, saveDB };
+exports.load = () => JSON.parse(fs.readFileSync(DB_FILE, "utf8"));
+exports.save = (db) => fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2));
