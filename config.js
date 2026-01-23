7c096f19af9a77e70e9e771b5107352b6fc1e288
kao678 <kaobas2536@gmail.com>
2026-01-23 17:17:47 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  PLAY_GROUP: "Gxxxxxxxx_play",
+  DEPOSIT_GROUP: "Gxxxxxxxx_deposit",
+  ADMIN_GROUP: "Gxxxxxxxx_admin",
+
+  RATE: {
+    NORMAL_WIN: 1,
+    NORMAL_LOSE: 3,
+    SPECIAL_456: 25,
+    TRIPLE: 100
+  }
+};
