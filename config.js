7707871bce339585cb16b9f2d9c0a77d2ed2e251
kao678 <kaobas2536@gmail.com>
2026-01-24 10:05:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  LINE_TOKEN: process.env.LINE_TOKEN,
-  LINE_SECRET: process.env.LINE_SECRET,
+  LINE_TOKEN: "YOUR_CHANNEL_ACCESS_TOKEN",
+  LINE_SECRET: "YOUR_CHANNEL_SECRET",
 
-  // ห้อง
-  PLAY_GROUP_ID: "PLAY_GROUP_ID_HERE",
-  DEPOSIT_GROUP_ID: "DEPOSIT_GROUP_ID_HERE",
-  ADMIN_GROUP_ID: "ADMIN_GROUP_ID_HERE",
+  PLAY_GROUP_ID: "ใส่ GID ห้องเล่น",
+  DEPOSIT_GROUP_ID: "ใส่ GID ห้องฝาก",
+  ADMIN_GROUP_ID: "ใส่ GID ห้องแอดมิน",
 
-  // รูปลูกเต๋า (ถ้าใช้)
-  DICE_URL: "https://your-domain/dice"
+  DICE_URL: "https://your-domain.com/dice"
 };
