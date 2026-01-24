b8d1b76532e8fa673d5de1fe756961f2a46c5a31
kao678 <kaobas2536@gmail.com>
2026-01-24 11:14:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  LINE_TOKEN: "YOUR_CHANNEL_ACCESS_TOKEN",
-  LINE_SECRET: "YOUR_CHANNEL_SECRET",
-
-  PLAY_GROUP_ID: "ใส่ GID ห้องเล่น",
-  DEPOSIT_GROUP_ID: "ใส่ GID ห้องฝาก",
-  ADMIN_GROUP_ID: "ใส่ GID ห้องแอดมิน",
-
-  DICE_URL: "https://your-domain.com/dice"
+  LINE_TOKEN: process.env.LINE_TOKEN,
+  LINE_SECRET: process.env.LINE_SECRET
 };
