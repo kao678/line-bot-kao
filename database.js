4f15b3945281d634a6f1f75f56aade8aed1648c2
kao678 <kaobas2536@gmail.com>
2026-01-24 10:48:16 +0700

    Update database.js

--- database.js
+++ database.js
@@ -7,15 +7,44 @@ function load() {
       users: {},
       bets: {},
       admins: [],
+      adminRoom: null,
+      playRoom: null,
+      depositRoom: null,
+      slips: {},
+      round: 1,
       config: {
         open: false,
         min: 50,
-        max: 2000
+        max: 2000,
+        waterLose: 1,
+        waterWin: 0,
+        freeWaterRounds: []
       },
       lastResult: null
     };
   }
-  return JSON.parse(fs.readFileSync(FILE, "utf8"));
+
+  const data = JSON.parse(fs.readFileSync(FILE, "utf8"));
+
+  // 🔐 กันพัง กรณีข้อมูลเก่า
+  data.users ??= {};
+  data.bets ??= {};
+  data.admins ??= [];
+  data.slips ??= {};
+  data.round ??= 1;
+  data.adminRoom ??= null;
+  data.playRoom ??= null;
+  data.depositRoom ??= null;
+  data.config ??= {
+    open: false,
+    min: 50,
+    max: 2000,
+    waterLose: 1,
+    waterWin: 0,
+    freeWaterRounds: []
+  };
+
+  return data;
 }
 
 function save(data) {
